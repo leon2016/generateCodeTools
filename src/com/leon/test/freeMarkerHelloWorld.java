90eb8fbc84a320f9736be04adc7186c163ee381d
Leon.Wan <1754235453@qq.com>
2017-04-24 13:56:33 +0800

    hello world !

--- src/com/leon/test/freeMarkerHelloWorld.java
+++ src/com/leon/test/freeMarkerHelloWorld.java
@@ -16,43 +16,43 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 public class freeMarkerHelloWorld {
-    public static void main(String[] args) { 
-        try { 
-          //创建一个合适的Configration对象 
-          Configuration configuration = new Configuration(); 
-          configuration.setDirectoryForTemplateLoading(new File(System.getProperty("user.dir") +"\\WebContent\\WEB-INF\\template")); 
-          configuration.setObjectWrapper(new DefaultObjectWrapper()); 
-          configuration.setDefaultEncoding("UTF-8");  //这个一定要设置，不然在生成的页面中 会乱码 
-          //获取或创建一个模版。 
-          Template template = configuration.getTemplate("hello_world.html"); 
-          Map<String, Object> paramMap = new HashMap<String, Object>(); 
-          paramMap.put("description", "我正在学习使用Freemarker生成静态文件！"); 
-           
-          List<String> nameList = new ArrayList<String>(); 
-          nameList.add("陈靖仇"); 
-          nameList.add("玉儿"); 
-          nameList.add("宇文拓"); 
-          paramMap.put("nameList", nameList); 
-           
-          Map<String, Object> weaponMap = new HashMap<String, Object>(); 
-          weaponMap.put("first", "轩辕剑"); 
-          weaponMap.put("second", "崆峒印"); 
-          weaponMap.put("third", "女娲石"); 
-          weaponMap.put("fourth", "神农鼎"); 
-          weaponMap.put("fifth", "伏羲琴"); 
-          weaponMap.put("sixth", "昆仑镜"); 
-          weaponMap.put("seventh", null); 
-          paramMap.put("weaponMap", weaponMap); 
-           
-          Writer writer = new OutputStreamWriter(new FileOutputStream("success.html"),"UTF-8"); 
-          template.process(paramMap, writer); 
-           
-          System.out.println("恭喜，生成成功~~"); 
-        } catch (IOException e) { 
-          e.printStackTrace(); 
-        } catch (TemplateException e) { 
-          e.printStackTrace(); 
-        } 
-         
-      } 
+    public static void main(String[] args) {
+        try {
+            // 1.创建一个合适的Configration对象
+            Configuration configuration = new Configuration();
+            configuration.setDirectoryForTemplateLoading(new File(System.getProperty("user.dir")
+                    + "\\WebContent\\WEB-INF\\template"));
+            configuration.setObjectWrapper(new DefaultObjectWrapper());
+            configuration.setDefaultEncoding("UTF-8"); // 这个一定要设置，不然在生成的页面中 会乱码
+            // 2.获取或创建一个模版。
+            Template template = configuration.getTemplate("hello_world.html");
+            // 3.生成模板数据Map
+            Map<String, Object> paramMap = new HashMap<String, Object>(); // 输出普通变量值
+            paramMap.put("text", "使用Freemarker生成静态文件！");
+
+            List<String> testList = new ArrayList<String>(); // list标签迭代list
+            testList.add("A");
+            testList.add("B");
+            testList.add("C");
+            testList.add("D");
+            paramMap.put("testList", testList);
+
+            Map<String, Object> testMap = new HashMap<String, Object>();// list标签迭代map
+            testMap.put("key1", "value1");
+            testMap.put("key2", "value2");
+            testMap.put("key3", null);
+            testMap.put("key4", "value3");
+            paramMap.put("testMap", testMap);
+            // 5.创建OutputStreamWriter对象生成文件
+            Writer writer = new OutputStreamWriter(new FileOutputStream("success.html"), "UTF-8");
+            template.process(paramMap, writer);
+
+            System.out.println("恭喜，生成成功~~");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
